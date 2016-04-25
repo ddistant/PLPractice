4fa13c32b82fe663bfd2cfb49158c276214b4762
Daniel Distant <daniel.distant1@gmail.com>
2016-04-25 00:37:02 -0400

    created clockView, working more on programmatic UI

--- PLPractice/SecondViewController.m
+++ PLPractice/SecondViewController.m
@@ -10,6 +10,12 @@
 
 @interface SecondViewController ()
 
+@property (nonatomic) ALView *clockView;
+@property (nonatomic) UIView *whiteView;
+@property (nonatomic) BOOL didSetupClockViewConstraints;
+
+@property (nonatomic) BOOL didSetupTickViews;
+
 @end
 
 @implementation SecondViewController
@@ -17,7 +23,71 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSLog(@"second view loaded");
+    self.view.backgroundColor = [UIColor lightGrayColor];
+    [self createViews];
+
+//    [self setupClockViewConstraints];
+}
+
+-(void) createViews {
+    
+    //clock view - most outside layer
+    
+    self.clockView = [ALView newAutoLayoutView];
+    self.clockView.backgroundColor = [UIColor blackColor];
+    [self.view addSubview:self.clockView];
+    self.clockView.layer.cornerRadius = 150;
+    [self.clockView autoCenterInSuperview];
+    [self.clockView autoSetDimensionsToSize:CGSizeMake(300, 300)];
+    
+    //next layer in - white
+    
+    self.whiteView = [ALView newAutoLayoutView];
+    self.whiteView.backgroundColor = [UIColor whiteColor];
+    [self.clockView addSubview:self.whiteView];
+    [self.whiteView autoSetDimensionsToSize:CGSizeMake(290, 290)];
+    [self.whiteView autoCenterInSuperview];
+    self.whiteView.layer.cornerRadius = 145;
+    
+    //create tick views
+    
+    [self createTickViews];
+    
+}
+
+-(void) setupClockViewConstraints {
+    
+    if (!self.didSetupClockViewConstraints) {
+        [NSLayoutConstraint autoCreateAndInstallConstraints:^{
+            [self.clockView autoCenterInSuperview];
+            [self.clockView autoSetDimensionsToSize:CGSizeMake(150, 150)];
+        }];
+        
+        self.didSetupClockViewConstraints = YES;
+    }
+}
+
+-(void) createTickViews {
+    
+    for (int i = 1; i < 5; i++) {
+        
+        UIView *tickView = [[UIView alloc] init];
+        tickView.backgroundColor = [UIColor redColor];
+        [self.whiteView addSubview:tickView];
+        [tickView autoSetDimensionsToSize:CGSizeMake(i, 12.0)];
+        [tickView autoCenterInSuperview];
+        tickView.transform = CGAffineTransformMakeTranslation(0.0, 130);
+        [self rotateView:self.clockView];
+        
+    }
+}
+
+-(void) rotateView:(UIView *)view {
+    
+    CGFloat angle = M_PI;
+    CGAffineTransform rotateTransform = CGAffineTransformRotate(view.transform, angle);
+    view.transform = rotateTransform;
+
 }
 
 - (void)didReceiveMemoryWarning {
